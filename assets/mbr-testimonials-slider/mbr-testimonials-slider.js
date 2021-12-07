0106241428041514b57e4a01832243b9a99d7667
zoubator <59017458+zoubator@users.noreply.github.com>
2021-12-07 07:46:57 +0100

    create github pages

--- assets/mbr-testimonials-slider/mbr-testimonials-slider.js
+++ assets/mbr-testimonials-slider/mbr-testimonials-slider.js
@@ -1,37 +1,3 @@
-function setActiveCarouselItem(card){
-   var $target = $(card).find('.carousel-item:first');
-   $target.addClass('active');
-} 
-function initTestimonialsCarousel(card){
-    var $target = $(card),
-        $carouselID = $target.attr('ID') +"-carousel"; 
 
-    $target.find('.carousel').attr('id',$carouselID);
-    $target.find('.carousel-controls a').attr('href','#'+$carouselID);
-    $target.find('.carousel-indicators li').attr('data-target','#'+$carouselID);
-    setActiveCarouselItem($target);  
-}
-
-// Mobirise initialization
-var isBuilder = $('html').hasClass('is-builder');
-if (isBuilder) {
-    $(document).on('add.cards', function(event) {
-        if($(event.target).hasClass('testimonials-slider')){
-            initTestimonialsCarousel(event.target);
-        }
-
-    }).on('changeParameter.cards', function(event, paramName, value) {
-        if (paramName === 'testimonialsSlides') {
-            if ($(event.target).find('.carousel-item.active').length==0) {
-                setActiveCarouselItem(event.target);
-            }
-        }
-    });;
-} else{
-    if(typeof window.initTestimonialsPlugin === 'undefined'){
-        window.initTestimonialsPlugin = true;
-        $('.testimonials-slider').each(function(){
-            initTestimonialsCarousel(this);
-        });      
-    }
-}
+function setActiveCarouselItem(a){$(a).find(".carousel-item:first").addClass("active")}function initTestimonialsCarousel(a){a=$(a);var b=a.attr("ID")+"-carousel";a.find(".carousel").attr("id",b);a.find(".carousel-controls a").attr("href","#"+b);a.find(".carousel-indicators li").attr("data-target","#"+b);setActiveCarouselItem(a)}var isBuilder=$("html").hasClass("is-builder");
+if(isBuilder)$(document).on("add.cards",function(a){$(a.target).hasClass("testimonials-slider")&&initTestimonialsCarousel(a.target)}).on("changeParameter.cards",function(a,b,c){"testimonialsSlides"===b&&0==$(a.target).find(".carousel-item.active").length&&setActiveCarouselItem(a.target)});else"undefined"===typeof window.initTestimonialsPlugin&&(window.initTestimonialsPlugin=!0,$(".testimonials-slider").each(function(){initTestimonialsCarousel(this)}));
